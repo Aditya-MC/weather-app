33e457999727ab168cd74aa8ff418b751c9b716e
Aditya-MC <adityamc364@gmail.com>
2025-08-02 19:15:33 +0530

    Initial Setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const apiKey = ""; 
+const searchBtn = document.getElementById("searchBtn");
+const currentLocBtn = document.getElementById("currentLocBtn");
+const cityInput = document.getElementById("cityInput");
+const weatherInfo = document.getElementById("weatherInfo");
+const forecastContainer = document.getElementById("forecast");
+const recentCities = document.getElementById("recentCities");
+
+
+
